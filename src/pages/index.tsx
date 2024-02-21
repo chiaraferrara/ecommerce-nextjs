9782fab2f4fdccc364441f777ede5e82f446783b
c.ferrara <chiara.ferrara@stevejobs.academy>
2024-02-21 14:04:39 +0100

    feat(index.tsx): map of products show products list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { myContext } from "./ContextProvider";
 
 export default function Home() {
-    
+    const { products } = useContext(myContext);
   return (
-    <>
+   <>
     <h1>Home</h1>
-    </>
+    <div> 
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+          <img src={product.image} alt={product.title} />
+        </div>
+      ))} 
+      </div>  
+      </>
   );
 }
